962dcb6aa39627300ab2ead98063e649b24976e6
amanji <ghaderi.amanj@gmail.com>
2022-06-19 20:54:44 +0430

    some ui added

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,6 +1,7 @@
 import React from "react";
-import Link from "next/link";
-
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 import Button from "../ui/Button";
 
 import classes from "./EventItem.module.css";
@@ -23,14 +24,22 @@ function EventItem({ title, image, date, location, id }) {
         <div className={classes.summery}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address> {formatedAddress} </address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Button link={exploreLink}> Explore event </Button>
+          <Button link={exploreLink}>
+            {" "}
+            <span> Explore event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />{" "}
+            </span>
+          </Button>
         </div>
       </div>
     </li>
