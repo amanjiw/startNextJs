3f8f13dcd0b291d378952f1664f8ad3e3943bd31
amanji <ghaderi.amanj@gmail.com>
2022-06-23 11:29:52 +0430

    filter events added

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,6 +1,41 @@
 import React from "react";
+import { useRouter } from "next/router";
 
+import { getFilteredEvents } from "../../dummy-data";
+
+// ## =>
 function FilteredEventsPage() {
+  const router = useRouter();
+  const filteredData = router.query.slug;
+
+  if (!filteredData) {
+    <p className="center">loading :) </p>;
+  }
+
+  const filteredYear = Number(filteredData[0]);
+  const filteredMonth = Number(filteredData[1]);
+
+  if (
+    isNaN(filteredYear) ||
+    isNaN(filteredMonth) ||
+    filteredYear > 2030 ||
+    filteredYear < 2021 ||
+    filteredMonth < 1 ||
+    filteredMonth > 12
+  ) {
+    return <p>Invalide Filter</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: filteredYear,
+    month: filteredMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No Events Found :(</p>;
+  }
+
+  //
   return (
     <>
       <h1>Slug page...</h1>
